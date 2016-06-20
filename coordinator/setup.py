f65476f5b56389c173367ccb7d356416276a6f73
mat <emkor93@gmail.com>
2016-06-20 20:03:09 +0200

    added structure for coordinator module

---
+++ coordinator/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(name='audiopyle_coordinator',
+      version='0.1.0',
+      description='Coordinator module for audiopyle, caring about selecting files to analysis',
+      author='Mateusz Korzeniowski',
+      author_email='emkor93@gmail.com',
+      url='',
+      packages=find_packages()
+      )
