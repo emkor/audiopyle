604504c26510755e1b59889f9e0ed0884875365e
urxvtcd <marcinchareza@gmail.com>
2016-07-14 15:59:54 +0200

    path + filename operations now done through FileAccessor

--- xtracter/xtracter/service/audio_file_converter.py
+++ xtracter/xtracter/service/audio_file_converter.py
@@ -1,3 +1,4 @@
+from commons.service.file_accessor import FileAccessor
 from pydub import AudioSegment
 
 DEFAULT_TARGET_CHANNEL_NUMBER = 1
@@ -13,13 +14,21 @@ class AudioFileConverter(object):
     def convert(self, input_file):
         self.input_file = input_file
         self.output_file = self.input_file + ".wav"
-        self.full_input_path = self.input_dir + self.input_file
-        self.full_output_path = self.output_dir + self.output_file
+
+        self.full_input_path = FileAccessor.join(
+            self.input_dir, self.input_file)
+
+        self.full_output_path = FileAccessor.join(
+            self.output_dir, self.output_file)
 
         self.song = AudioSegment.from_mp3(self.full_input_path)
+
         self.song_mono = self.song.set_channels(DEFAULT_TARGET_CHANNEL_NUMBER)
+
         self.song_sample_rate = self.song_mono.set_frame_rate(
             DEFAULT_TARGET_SAMPLE_RATE)
+
         self.song_sample_width = self.song_sample_rate.set_sample_width(
             DEFAULT_TARGET_SAMPLE_WIDTH)
+
         self.song_sample_width.export(self.full_output_path, format="wav")
