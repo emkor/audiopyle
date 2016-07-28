ef9d54b1e4c4cf63ec195d482dc00d17e6c11eb0
urxvtcd <marcinchareza@gmail.com>
2016-07-28 19:52:32 +0200

    Change dir for converter test output

--- xtracter/xtracter/test/test_audio_file_converter.py
+++ xtracter/xtracter/test/test_audio_file_converter.py
@@ -12,14 +12,14 @@ class AudioFileConverterIntegrationTest(unittest.TestCase):
     def setUp(self):
         self.converter = AudioFileConverter(
             XtracterUtils.get_test_resources_path(),
-            XtracterUtils.get_wav_file_path())
+            XtracterUtils.get_test_resources_path())
 
     def test_should_be_equal(self):
         self.converter.convert(XtracterUtils.get_test_mp3_file_name())
 
         song = AudioSegment.from_wav(
             FileAccessor.join(
-                XtracterUtils.get_wav_file_path(),
+                XtracterUtils.get_test_resources_path(),
                 XtracterUtils.get_test_mp3_file_name() + ".wav"))
 
         assert_that(song).is_instance_of(AudioSegment)
