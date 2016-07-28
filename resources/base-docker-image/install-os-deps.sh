d82dcf0575740c48484a7c768d819b5209cf6ff4
urxvtcd <marcinchareza@gmail.com>
2016-07-28 14:26:53 +0200

    Add ffmpeg to installed packages

--- resources/base-docker-image/install-os-deps.sh
+++ resources/base-docker-image/install-os-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y wget build-essential libsqlite3-dev zlib1g-dev libncurses5-dev libgdbm-dev libbz2-dev libreadline-gplv2-dev libssl-dev libdb-dev
+apt-get install -y wget build-essential libsqlite3-dev zlib1g-dev libncurses5-dev libgdbm-dev libbz2-dev libreadline-gplv2-dev libssl-dev libdb-dev libav-tools
