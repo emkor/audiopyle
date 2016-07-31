f78d601d4923971dd5765a5f1ad6ed7d5438dda3
urxvtcd <marcinchareza@gmail.com>
2016-07-31 16:11:50 +0200

    Add basic remote audio file model

---
+++ coordinator/coordinator/model/remote_audio_file.py
@@ -0,0 +1,5 @@
+class RemoteAudioFile(object):
+    def __init__(self, name, size, upload_timestamp):
+        self.name = name
+        self.size = size
+        self.upload_timestamp = upload_timestamp
