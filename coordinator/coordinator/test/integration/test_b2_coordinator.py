e52a75aacb2a605a6dc7eefce8e39b974e053094
urxvtcd <marcinchareza@gmail.com>
2016-08-01 19:02:47 +0200

    Refactor tests

--- coordinator/coordinator/test/integration/test_b2_coordinator.py
+++ coordinator/coordinator/test/integration/test_b2_coordinator.py
@@ -7,12 +7,11 @@ from coordinator.service.b2_coordinator import B2Coordinator
 
 class B2CoordinatorIntegrationTest(unittest.TestCase):
     def setUp(self):
-        coordinator = B2Coordinator()
-        self.files = coordinator.get_remote_audio_files()
+        self.coordinator = B2Coordinator()
 
     def test_should_get_remote_audio_file(self):
-        assert_that(self.files[0]).is_instance_of(RemoteFileMeta)
-        file_string = "RemoteFileMeta: file name: "\
-            "test/102bpm_drum_loop_mono_44.1k.wav, "\
-            "file size: 207916, upload timestamp: 1467569056000."
-        assert_that(str(self.files[0])).is_equal_to(file_string)
+        files = self.coordinator.get_remote_audio_files()
+        assert_that(len(files)).is_equal_to(1)
+        assert_that(files[0]).is_instance_of(RemoteFileMeta)
+        assert_that(str(files[0])).contains("102bpm_drum_loop_mono_44.1k.wav",
+                                            "207916")
--- coordinator/coordinator/test/test_b2_coordinator.py
+++ coordinator/coordinator/test/test_b2_coordinator.py
@@ -7,9 +7,9 @@ from coordinator.service.b2_coordinator import B2Coordinator
 
 class TestB2Coordinator(unittest.TestCase):
     def setUp(self):
-        coordinator = B2Coordinator()
-        coordinator.audio_provider = Mock()
-        coordinator.audio_provider.get_file_infos.return_value = \
+        self.coordinator = B2Coordinator()
+        self.coordinator.audio_provider = Mock()
+        self.coordinator.audio_provider.get_file_infos.return_value = \
             [{u'contentType': u'audio',
               u'fileName': u'audio-file',
               u'size': 100,
@@ -18,9 +18,8 @@ class TestB2Coordinator(unittest.TestCase):
               u'fileName': u'some-file',
               u'size': 300,
               u'uploadTimestamp': 400}]
-        self.files = coordinator.get_remote_audio_files()
 
     def test_should_filter_audio_file(self):
-        file_string = "RemoteFileMeta: file name: "\
-            "audio-file, file size: 100, upload timestamp: 200."
-        assert_that(str(self.files[0])).is_equal_to(file_string)
+        files = self.coordinator.get_remote_audio_files()
+        assert_that(len(files)).is_equal_to(1)
+        assert_that(str(files[0])).contains("audio-file", "100", "200")
