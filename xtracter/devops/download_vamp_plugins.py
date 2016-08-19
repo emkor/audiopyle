1d6a40f6f689471338ed38e5af1b1a964fa61c36
Ziemnior <djpitugta@gmail.com>
2016-08-19 05:41:19 +0200

    an attempt to fix paths

--- xtracter/devops/download_vamp_plugins.py
+++ xtracter/devops/download_vamp_plugins.py
@@ -4,7 +4,7 @@ import os
 from b2.api import B2Api
 from b2.download_dest import DownloadDestLocalFile
 from b2.progress import make_progress_listener
-from commons.utils import logging_setup
+from commons.utils.logging_setup import myGetLogger
 
 # -------------------------- CONFIG --------------------------
 REALM = 'production'
