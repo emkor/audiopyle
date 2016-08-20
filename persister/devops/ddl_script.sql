2720648bd850a8ec3fe30e67c4460f7e778b4f57
emkor <mkorzeniowski93@gmail.com>
2016-08-20 14:39:13 +0200

    added DDL script for MySQL

---
+++ persister/devops/ddl_script.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE audiopyle;
+
+USE audiopyle;
+
+CREATE TABLE plugin (
+  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  plugin_key VARCHAR(100) NOT NULL,
+  output VARCHAR(50) NOT NULL,
+  CONSTRAINT plugin_unique UNIQUE (plugin_key, output)
+);
+
+CREATE TABLE track (
+  uuid BINARY(16) NOT NULL PRIMARY KEY,
+  filename VARCHAR(256) NOT NULL,
+  bit_depth INT UNSIGNED NOT NULL,
+  sample_rate INT UNSIGNED NOT NULL,
+  frames_count BIGINT UNSIGNED NOT NULL,
+  channels_count INT UNSIGNED NOT NULL
+);
+
+CREATE TABLE segment (
+  id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  track_uuid BINARY(16) NOT NULL,
+  offset BIGINT UNSIGNED NOT NULL,
+  length BIGINT UNSIGNED NOT NULL,
+  FOREIGN KEY (track_uuid) REFERENCES track(uuid)
+);
+
+CREATE TABLE feature (
+  id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  plugin_id INT UNSIGNED NOT NULL,
+  segment_id BIGINT UNSIGNED NOT NULL,
+  FOREIGN KEY (plugin_id) REFERENCES plugin(id),
+  FOREIGN KEY (segment_id) REFERENCES segment(id),
+  CONSTRAINT feature_unique UNIQUE (plugin_id, segment_id)
+);
+
+CREATE TABLE raw_feature (
+  id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  feature_id BIGINT UNSIGNED NOT NULL,
+  timestamp FLOAT NOT NULL,
+  label VARCHAR(100),
+  FOREIGN KEY (feature_id) REFERENCES feature(id)
+);
+
+CREATE TABLE raw_feature_value (
+  id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  raw_feature_id BIGINT UNSIGNED NOT NULL,
+  value FLOAT,
+  FOREIGN KEY (raw_feature_id) REFERENCES raw_feature(id)
+);
+
+
