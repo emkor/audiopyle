64502f9b2fcbe6ae25e5e2d1350ca0b3a15a0ba0
emkor <mkorzeniowski93@gmail.com>
2016-08-23 20:17:24 +0200

    fixed tests according to implementation updated

--- commons/commons/test/integration/test_b2_audio_provider.py
+++ commons/commons/test/integration/test_b2_audio_provider.py
@@ -1,5 +1,7 @@
 import unittest
 from assertpy import assert_that
+
+from commons.model.remote_file_meta import RemoteFileMeta
 from commons.model.remote_file_source import B2Config
 from commons.provider.b2_audio_provider import B2AudioProvider
 from commons.service.file_accessor import FileAccessor
@@ -9,36 +11,32 @@ from commons.utils.constant import AudiopyleConst
 
 class B2AudioProviderIntegrationTest(unittest.TestCase):
     def setUp(self):
-        b2_test_config = B2Config(AudiopyleConst.B2_ACCOUNT_ID, AudiopyleConst.B2_APPLICATION_KEY,
-                                  AudiopyleConst.B2_RESOURCES_BUCKET)
+        self.b2_test_config = B2Config(AudiopyleConst.B2_ACCOUNT_ID, AudiopyleConst.B2_APPLICATION_KEY,
+                                       AudiopyleConst.B2_RESOURCES_BUCKET)
         local_cache_dir = OsEnvAccessor.get_env_variable(AudiopyleConst.PROJECT_HOME_ENV)
-        self.audio_provider = B2AudioProvider(b2_test_config, local_cache_dir)
-
-    def test_bucket_should_exits(self):
-        is_connected_to_bucket = self.audio_provider.is_connected()
-        assert_that(is_connected_to_bucket).is_true()
+        self.audio_provider = B2AudioProvider(local_cache_dir)
 
     def test_file_should_exists(self):
-        test_file_exists = self.audio_provider.exists(AudiopyleConst.B2_TEST_FILE_PATH)
+        test_file_exists = self.audio_provider.remote_file_path_exists(self.b2_test_config,
+                                                                       AudiopyleConst.B2_TEST_FILE_PATH)
         assert_that(test_file_exists).is_true()
 
     def test_made_up_file_should_not_exists(self):
-        test_file_exists = self.audio_provider.exists("smthng_smthng/anyfile.txt")
+        made_up_file_path = "smthng_smthng/anyfile.txt"
+        test_file_exists = self.audio_provider.remote_file_path_exists(self.b2_test_config, made_up_file_path)
         assert_that(test_file_exists).is_false()
 
-    def test_remote_file_paths_should_contain_test_file(self):
-        remote_file_paths = self.audio_provider.get_filepaths_list()
-        assert_that(remote_file_paths).contains(AudiopyleConst.B2_TEST_FILE_PATH)
+    def test_get_files_raw_info_should_contain_anything(self):
+        file_infos = self.audio_provider.get_raw_file_infos(self.b2_test_config)
+        assert_that(file_infos).is_not_none().is_not_empty()
 
-    def test_remote_file_paths_should_not_contain_made_up_file(self):
-        remote_file_paths = self.audio_provider.get_filepaths_list()
-        assert_that(remote_file_paths).does_not_contain("smthng_smthng/anyfile.txt")
+    def test_get_files_raw_info_should_contain_test_file_meta(self):
+        expected_remote_file = RemoteFileMeta(AudiopyleConst.B2_TEST_FILE_PATH, AudiopyleConst.B2_TEST_FILE_SIZE,
+                                              AudiopyleConst.B2_TEST_FILE_UPLOAD_TIMESTAMP)
+        remote_file_mets = self.audio_provider.get_remote_file_metas(self.b2_test_config)
+        self.assertIn(expected_remote_file, remote_file_mets)
 
     def test_should_download_remote_file(self):
-        local_file_path = self.audio_provider.download(AudiopyleConst.B2_TEST_FILE_PATH)
+        local_file_path = self.audio_provider.download(self.b2_test_config, AudiopyleConst.B2_TEST_FILE_PATH)
         assert_that(FileAccessor.exists(local_file_path)).is_true()
         FileAccessor.remove_file(local_file_path)
-
-    def test_get_files_info_should_contain_anything(self):
-        file_infos = self.audio_provider.get_file_infos()
-        assert_that(file_infos).is_not_none().is_not_empty()
