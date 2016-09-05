6a5023496a171a163b2668e193c6871fa709148c
mkorzeni <mateusz.korzeniowski@nokia.com>
2016-09-05 18:06:17 +0200

    added try-catch blocks in object serialization

--- commons/commons/model/analysis_task.py
+++ commons/commons/model/analysis_task.py
@@ -12,11 +12,15 @@ class AnalysisTask(object):
         :type analysis_task_dict: dict
         :rtype: commons.model.analysis_task.AnalysisTask
         """
-        remote_file_meta = RemoteFileMeta(**(analysis_task_dict.get("remote_file_meta")))
-        remote_file_source = RemoteFileSource(**(analysis_task_dict.get("remote_file_source")))
-        created_timestamp = analysis_task_dict.get("created_timestamp")
-        return AnalysisTask(remote_file_meta=remote_file_meta, remote_file_source=remote_file_source,
-                            created_timestamp=created_timestamp)
+        try:
+            remote_file_meta = RemoteFileMeta(**(analysis_task_dict.get("remote_file_meta")))
+            remote_file_source = RemoteFileSource(**(analysis_task_dict.get("remote_file_source")))
+            created_timestamp = analysis_task_dict.get("created_timestamp")
+            return AnalysisTask(remote_file_meta=remote_file_meta, remote_file_source=remote_file_source,
+                                created_timestamp=created_timestamp)
+        except Exception as e:
+            print("Could not decode dict to AnalysisTask. Input: {} Details: {}".format(analysis_task_dict, e))
+            return None
 
     def __init__(self, remote_file_meta, remote_file_source, created_timestamp=None):
         """
