e3b3ffc5656a31f9358fea7b8b6501f4953640f9
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-01-06 14:28:22 +0100

    updated description and install script

--- devops/audiopyle_base/install_python2_base_modules.sh
+++ devops/audiopyle_base/install_python2_base_modules.sh
@@ -3,5 +3,8 @@
 # install pip
 wget "https://bootstrap.pypa.io/get-pip.py" && python get-pip.py
 
-# install setuptools
+# install setuptools - the recommended way
 wget http://peak.telecommunity.com/dist/ez_setup.py && python ez_setup.py
+
+# get-pip cleanup
+rm -rf ./get-pip.py
