be3428f4091d62235b05102d1be2a9f5916c93ea
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-02-23 22:22:59 +0100

    new way of serializing

--- extracter/extracter/exctracter_api.py
+++ extracter/extracter/exctracter_api.py
@@ -1,6 +1,6 @@
 import cherrypy
 
-from commons.api_utils import to_json
+from commons.api_utils import jsonify
 from commons.file_system import list_files
 from extracter.vampy_plugin_provider import list_vampy_plugins
 
@@ -10,22 +10,25 @@ TMP_DIR = "/audio_tmp"
 
 class ExtracterApi(object):
     @cherrypy.expose
+    @jsonify
     def index(self):
         """
         :rtype: str
         """
-        return to_json({"api": "extracter", "status": "ok"})
+        return {"api": "extracter", "status": "ok"}
 
     @cherrypy.expose
+    @jsonify
     def plugins(self):
         """
         :rtype: str
         """
-        return to_json(list_vampy_plugins())
+        return list_vampy_plugins()
 
     @cherrypy.expose
+    @jsonify
     def audio(self):
         """
         :rtype: str
         """
-        return to_json(list_files(AUDIO_FILES_DIR))
+        return list_files(AUDIO_FILES_DIR)
