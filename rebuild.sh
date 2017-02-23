816bff5469d9d231b98e5cc7a27758bb686bb57d
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-02-23 19:12:47 +0100

    added script for rebuilding docker images and running

---
+++ rebuild.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+docker stop test-extracter && docker rm test-extracter
+
+docker rmi endlessdrones/audiopyle-commons
+docker rmi endlessdrones/audiopyle-extracter
+
+docker build -t "endlessdrones/audiopyle-commons" ./commons
+docker build -t "endlessdrones/audiopyle-extracter" ./extracter
+
+docker run -v /home/mkorzeni/vamp:/root/vamp -v /home/mkorzeni/projects/audiopyle/resources:/audio -p 127.0.0.1:8080:8080 --name "test-extracter" endlessdrones/audiopyle-extracter
