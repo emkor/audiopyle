ffcfd59f1e444a4dc706d435433ed50c23503917
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-02-23 22:06:08 +0100

    fix serialization

--- commons/commons/api_utils.py
+++ commons/commons/api_utils.py
@@ -8,6 +8,18 @@ def to_json(to_serialize):
     :type to_serialize: object | Enum
     :rtype: str
     """
+    try:
+        return json.dumps(to_serialize)
+    except Exception as e:
+        print(e)
+    return json.dumps(_make_serializable(to_serialize))
+
+
+def _make_serializable(to_serialize):
+    """
+    :type to_serialize: object
+    :rtype: basestring | dict | list | int | float
+    """
     if isinstance(to_serialize, datetime.datetime):
         serializable = to_serialize.isoformat()
     elif isinstance(to_serialize, Decimal):
@@ -16,4 +28,4 @@ def to_json(to_serialize):
         serializable = list(to_serialize)
     else:
         serializable = to_serialize.__dict__
-    return json.dumps(serializable)
+    return serializable
