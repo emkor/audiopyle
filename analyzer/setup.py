2603adb206a15b58c7380937b14ed36eb0b064e2
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-05-23 22:18:13 +0200

    added dummy celery node

---
+++ analyzer/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='audiopyle-analyzer',
+      version='0.1.0',
+      description='Analyzer functionality for audiopyle app',
+      author='Mateusz Korzeniowski',
+      author_email='mkorzeniowski93@gmail.com',
+      url='',
+      packages=find_packages()
+      )
