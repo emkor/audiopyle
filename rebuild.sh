9a6ad579c22279edb0d23caaa3fd76cc6c03391a
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-05-24 21:57:47 +0200

    fixed build scripts

--- rebuild.sh
+++ rebuild.sh
@@ -6,6 +6,6 @@ docker rmi endlessdrones/audiopyle-commons
 docker rmi endlessdrones/audiopyle-extracter
 
 docker build -t "endlessdrones/audiopyle-commons" ./commons
-docker build -t "endlessdrones/audiopyle-api" ./extracter
+docker build -t "endlessdrones/audiopyle-coordinator" ./extracter
 
-docker run -v /home/mkorzeni/vamp:/root/vamp -v /home/mkorzeni/projects/audiopyle/resources:/audio -p 127.0.0.1:8080:8080 --name "test-api" endlessdrones/audiopyle-extracter
+docker run -v /home/mkorzeni/vamp:/root/vamp -v /home/mkorzeni/projects/audiopyle/resources:/audio -p 127.0.0.1:8080:8080 --name "test-coordinator" endlessdrones/audiopyle-extracter
