8b06ab7c1404ad0bf2c2bda45ee7ffced10366b4
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-05-24 19:12:20 +0200

    added redis for storing results and added link to compose

--- analyzer/tasks.py
+++ analyzer/tasks.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('tasks', broker='pyamqp://rabbitmq:rabbitmq@rabbit1//')
+app = Celery('tasks', broker='pyamqp://rabbitmq:rabbitmq@rabbit1//', backend="redis://redis1")
 
 
 @app.task
