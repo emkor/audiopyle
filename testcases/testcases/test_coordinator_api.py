44bd039698ce0ea7cdd0a1827badd9c70ef7e8ef
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-07-15 01:28:44 +0200

    checking if build is red when testcases exits with 1

--- testcases/testcases/test_coordinator_api.py
+++ testcases/testcases/test_coordinator_api.py
@@ -12,4 +12,4 @@ class CoordinatorApiTest(TestCase):
     def test_should_return_ok_after_boot(self):
         response = keep_polling_until(url=self.coordinator_url, expected_status=200, timeout=self.timeout)
         assert_that(response.status_code).is_equal_to(200)
-        assert_that(response.json()).is_equal_to("ok")
+        assert_that(response.json()).is_equal_to("nok")
