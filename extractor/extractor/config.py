db40a26d8c9d816994385b2b494dd288d5cdb46d
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-08-27 20:41:38 +0200

    added parsing env variable to given type

--- extractor/extractor/config.py
+++ extractor/extractor/config.py
@@ -9,11 +9,11 @@ accept_content = ['json']
 timezone = 'Europe/Warsaw'
 enable_utc = True
 
-worker_concurrency = get_environment_variable("EXTRACTION_CONCURRENCY",
+worker_concurrency = get_environment_variable("EXTRACTION_CONCURRENCY", int,
                                               default=2)  # concurrent tasks at a time per worker
 task_acks_late = True
 
-broker_pool_limit = get_environment_variable("EXTRACTION_BROKER_CONN_POOL_SIZE", default=4)
+broker_pool_limit = get_environment_variable("EXTRACTION_BROKER_CONN_POOL_SIZE", int, default=4)
 
 worker_prefetch_multiplier = 1
 worker_max_tasks_per_child = 1
@@ -21,7 +21,7 @@ worker_max_memory_per_child = 128 * 1000  # 128 MB
 worker_send_task_events = True
 task_send_sent_event = True
 task_track_started = True
-task_soft_time_limit = get_environment_variable("EXTRACTION_SOFT_TIME_LIMIT_SECONDS",
+task_soft_time_limit = get_environment_variable("EXTRACTION_SOFT_TIME_LIMIT_SECONDS", float,
                                                 default=2 * 60)  # exception within task is raised after: 2 minutes
-task_time_limit = get_environment_variable("EXTRACTION_HARD_TIME_LIMIT_SECONDS",
+task_time_limit = get_environment_variable("EXTRACTION_HARD_TIME_LIMIT_SECONDS", float,
                                            default=5 * 60)  # worker process is killed after: 5 minutes
