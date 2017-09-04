72ba8271e4ecb26f1ccd72e2b38a04e796623552
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-09-04 18:49:48 +0200

    corrected abstraction for API

--- commons/commons/abstractions/api.py
+++ commons/commons/abstractions/api.py
@@ -1,8 +1,9 @@
 from logging import Logger
+from typing import Optional, Any, Text, Dict, Callable
 
 import cherrypy
 
-from commons.abstractions.api_model import ApiRequest, ApiResponse, HttpMethod
+from commons.abstractions.api_model import ApiRequest, ApiResponse, HttpMethod, ClientError, ServerError
 from commons.utils.conversion import seconds_between
 from commons.utils.logger import get_logger
 from commons.utils.serialization import to_json
@@ -32,40 +33,45 @@ class AudiopyleRestApi(object):
 
     @cherrypy.tools.accept(media=TEXT_PLAIN_HEADER)
     def GET(self, **query_params):
-        request = ApiRequest(url=cherrypy.url(), method=HttpMethod.GET, query_params=query_params,
-                             headers=cherrypy.request.headers, payload={})
-        response = self.get(request)
-        response_json = to_json(response.payload)
-        self._log_api_call(request, response)
-        return response_json
+        return self._handle_request(cherrypy.url(), HttpMethod.GET, self.get, query_params,
+                                    cherrypy.request.headers, None)
 
     @cherrypy.tools.accept(media=APPLICATION_JSON_HEADER)
     @cherrypy.tools.json_in()
     def POST(self, **query_params):
-        request = ApiRequest(url=cherrypy.url(), method=HttpMethod.POST, query_params=query_params,
-                             headers=cherrypy.request.headers, payload=cherrypy.request.json)
-        response = self.post(request)
-        response_json = to_json(response.payload)
-        self._log_api_call(request, response)
-        return response_json
+        return self._handle_request(cherrypy.url(), HttpMethod.POST, self.post, query_params,
+                                    cherrypy.request.headers, cherrypy.request.json)
 
     @cherrypy.tools.accept(media=APPLICATION_JSON_HEADER)
     def PUT(self, **query_params):
-        request = ApiRequest(url=cherrypy.url(), method=HttpMethod.POST, query_params=query_params,
-                             headers=cherrypy.request.headers, payload=cherrypy.request.json)
-        response = self.post(request)
-        response_json = to_json(response)
-        self._log_api_call(request, response)
-        return response_json
+        return self._handle_request(cherrypy.url(), HttpMethod.PUT, self.put, query_params,
+                                    cherrypy.request.headers, cherrypy.request.json)
 
     @cherrypy.tools.accept(media=TEXT_PLAIN_HEADER)
     def DELETE(self, **query_params):
-        request = ApiRequest(url=cherrypy.url(), method=HttpMethod.POST, query_params=query_params,
-                             headers=cherrypy.request.headers, payload=cherrypy.request.json)
-        response = self.delete(request)
-        response_json = to_json(response)
-        self._log_api_call(request, response)
-        return response_json
+        return self._handle_request(cherrypy.url(), HttpMethod.DELETE, self.delete, query_params,
+                                    cherrypy.request.headers, None)
+
+    def _handle_request(self,
+                        request_url: Text,
+                        http_method: HttpMethod,
+                        method_handler: Callable[ApiRequest, ApiResponse],
+                        query_params: Dict[Text, Any],
+                        headers: Dict[Text, Any],
+                        request_payload: Optional[Dict[Text, Any]] = None):
+        request = ApiRequest(url=request_url, method=http_method, query_params=query_params,
+                             headers=headers, payload=request_payload or {})
+        try:
+            response = method_handler(request)
+            response_json = to_json(response)
+            self._log_api_call(request, response)
+            return response_json
+        except ClientError as e:
+            self.logger.warning("Bad request to {}: {}, request: {}".format(self.__class__.__name__, e, request))
+            raise cherrypy.HTTPError(status=e.status_code, message=e.message)
+        except Exception as e:
+            self.logger.exception("Server error in {}: {}, request: {}".format(self.__class__.__name__, e, request))
+            raise cherrypy.HTTPError(status=500, message=e)
 
     def _log_api_call(self, api_request: ApiRequest, api_response: ApiResponse):
         serving_time = seconds_between(api_request.creation_time)
