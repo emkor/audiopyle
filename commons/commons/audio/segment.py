50fce32666acc4fcfeabaf72c69773ba2e586f40
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-09-14 19:03:57 +0200

    adding type annotations

--- commons/commons/audio/segment.py
+++ commons/commons/audio/segment.py
@@ -1,48 +1,31 @@
+from numpy.core.multiarray import ndarray
+
 from commons.abstractions.model import Model
+from commons.audio.file_meta import AudioFileMeta
 from commons.utils.conversion import frames_to_sec
 
 
 class AudioSegmentMeta(Model):
-    def __init__(self, source_file_meta, frame_from, frame_to):
-        """
-        Represents metadata of a part of an audio wave
-        :type source_file_meta: commons.audio.file_meta.AudioFileMeta
-        :type frame_from: int
-        :type frame_to: int
-        """
+    def __init__(self, source_file_meta: AudioFileMeta, frame_from: int, frame_to: int) -> None:
+        """Represents metadata of a part of an audio wave"""
         self.source_file_meta = source_file_meta
         self.frame_from = frame_from
         self.frame_to = frame_to
 
-    def length_frames(self):
-        """
-        :rtype: int
-        """
+    def length_frames(self) -> int:
         return abs(self.frame_to - self.frame_from)
 
-    def length_sec(self):
-        """
-        :rtype: float
-        """
+    def length_sec(self) -> float:
         return frames_to_sec(self.length_frames(), self.source_file_meta.sample_rate)
 
 
 class MonoAudioSegment(AudioSegmentMeta):
-    def __init__(self, source_file_meta, frame_from, frame_to, data):
-        """
-        Represents metadata of a part of an audio wave
-        :type source_file_meta: commons.audio.file_meta.AudioFileMeta
-        :type frame_from: int
-        :type frame_to: int
-        :type data: numpy.core.multiarray.ndarray
-        """
+    def __init__(self, source_file_meta: AudioFileMeta, frame_from: int, frame_to: int, data: ndarray) -> None:
+        """Represents metadata of a part of an audio wave"""
         super(MonoAudioSegment, self).__init__(source_file_meta, frame_from, frame_to)
         self.data = data
 
-    def get_meta(self):
-        """
-        :rtype: commons.audio.segment.AudioSegmentMeta
-        """
+    def get_meta(self) -> AudioSegmentMeta:
         return AudioSegmentMeta(self.source_file_meta, self.frame_from, self.frame_to)
 
     def __str__(self):
