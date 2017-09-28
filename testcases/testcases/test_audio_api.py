33df329326d345e0c1011d34d1e9a27c4ea43ce0
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-09-28 21:15:25 +0200

    simplified integration test

--- testcases/testcases/test_audio_api.py
+++ testcases/testcases/test_audio_api.py
@@ -13,14 +13,11 @@ class AudioApiTest(TestCase):
     def test_should_list_audio_file(self):
         expected_status_code = 200
         expected_audio_file_count = 2
-        expected_audio = [{'created_on': '2017-07-19T17:42:15', 'last_modification': '2017-07-19T17:42:15',
-                           'last_access': '2017-07-19T17:42:15', 'file_name': '/audio/102bpm_drum_loop_mono_44.1k.wav',
-                           'size': 207916}]
         response = requests.get(url=self.audio_api_url)
         assert_that(response.status_code).is_equal_to(expected_status_code)
         actual_response = response.json()
         assert_that(actual_response).is_length(expected_audio_file_count)
-        assert_that(actual_response[0].get("file_name")).is_equal_to(expected_audio[0].get("file_name"))
+        assert_that(actual_response[0].get("file_name")).is_not_empty()
         assert_that(actual_response[0].get("created_on")).is_not_empty()
         assert_that(actual_response[0].get("last_modification")).is_not_empty()
         assert_that(actual_response[0].get("last_access")).is_not_empty()
