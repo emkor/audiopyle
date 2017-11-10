3db21c2b7a75d0ca64563de052a8b54227668abe
emkor93 <emkor93@gmail.com>
2017-11-10 19:03:02 +0100

    did the same improvements to coordinator and extractor packages

--- extractor/setup.py
+++ extractor/setup.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name='audiopyle-extractor',
-      version='0.1.1',
-      description='Audio feature extraction functionality for audiopyle app',
-      author='Mateusz Korzeniowski',
-      author_email='mkorzeniowski93@gmail.com',
-      url='',
-      packages=find_packages()
-      )
+setup(
+    name='audiopyle-extractor',
+    version='0.2.0',
+    description='Audio feature extraction functionality for audiopyle app',
+    author='Mateusz Korzeniowski',
+    author_email='emkor93@gmail.com',
+    url='https://github.com/emkor/audiopyle',
+    packages=find_packages(),
+    install_requires=["pymysql", "celery[sqlalchemy]", "mutagen"]
+)
