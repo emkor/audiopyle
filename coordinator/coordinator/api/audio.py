d45eb9320b9f7a30c6719052b0bc27fb0e2951bd
emkor93 <emkor93@gmail.com>
2017-11-12 12:36:27 +0100

    corrected audio APIs

--- coordinator/coordinator/api/audio.py
+++ coordinator/coordinator/api/audio.py
@@ -1,24 +1,24 @@
-from mutagen.easyid3 import EasyID3
-
 from commons.abstractions.api import AudiopyleRestApi
 from commons.abstractions.api_model import ApiRequest, ApiResponse, HttpStatusCode
-from commons.audio.audio_tag import Id3Tag
-from commons.audio.file_meta_providing import read_wav_file_meta
-from commons.utils.file_system import list_files, AUDIO_FILES_DIR, concatenate_paths, TMP_DIR, remove_file
+from commons.audio.audio_tag_providing import read_id3_tag
+from commons.audio.file_meta_providing import read_wav_file_meta, read_mp3_file_meta
+from commons.utils.file_system import list_files, AUDIO_FILES_DIR, concatenate_paths, TMP_DIR, remove_file, \
+    extract_extension
 
 
 class AudioApi(AudiopyleRestApi):
     def get(self, request: ApiRequest) -> ApiResponse:
         absolute_files_paths = [concatenate_paths(AUDIO_FILES_DIR, f) for f in list_files(AUDIO_FILES_DIR)]
-        audio_files_metas = map(lambda p: read_wav_file_meta(p), absolute_files_paths)
-        return ApiResponse(HttpStatusCode.ok, list(audio_files_metas))
+        mp3_absolute_file_paths = [f for f in absolute_files_paths if extract_extension(f) == "mp3"]
+        audio_files_metas = list(map(lambda p: read_mp3_file_meta(p), mp3_absolute_file_paths))
+        return ApiResponse(HttpStatusCode.ok, audio_files_metas)
 
 
 class TmpAudioApi(AudiopyleRestApi):
     def get(self, request: ApiRequest) -> ApiResponse:
         absolute_files_paths = [concatenate_paths(TMP_DIR, f) for f in list_files(TMP_DIR)]
-        audio_files_metas = map(lambda p: read_wav_file_meta(p), absolute_files_paths)
-        return ApiResponse(HttpStatusCode.ok, list(audio_files_metas))
+        audio_files_metas = list(map(lambda p: read_wav_file_meta(p), absolute_files_paths))
+        return ApiResponse(HttpStatusCode.ok, audio_files_metas)
 
     def delete(self, request: ApiRequest):
         absolute_files_paths = [concatenate_paths(TMP_DIR, f) for f in list_files(TMP_DIR)]
@@ -32,6 +32,6 @@ class AudioTagApi(AudiopyleRestApi):
     def get(self, request: ApiRequest):
         audio_file_name = request.query_params.get("file")
         self.logger.info("Reading ID3 tags of {}...".format(audio_file_name))
-        full_audio_file_name = concatenate_paths(AUDIO_FILES_DIR, audio_file_name)
-        id3_tag = Id3Tag.from_easy_id3_object(EasyID3(full_audio_file_name))
+        audio_file_absolute_path = concatenate_paths(AUDIO_FILES_DIR, audio_file_name)
+        id3_tag = read_id3_tag(audio_file_absolute_path)
         return ApiResponse(HttpStatusCode.ok, id3_tag.serialize())
