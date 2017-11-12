0eddebba6181f76e00f7de87f3667a2cf44632b7
emkor93 <emkor93@gmail.com>
2017-11-12 13:05:23 +0100

    corrected audio APIs so they filter file types

--- coordinator/coordinator/api/audio.py
+++ coordinator/coordinator/api/audio.py
@@ -17,7 +17,8 @@ class AudioApi(AudiopyleRestApi):
 class TmpAudioApi(AudiopyleRestApi):
     def get(self, request: ApiRequest) -> ApiResponse:
         absolute_files_paths = [concatenate_paths(TMP_DIR, f) for f in list_files(TMP_DIR)]
-        audio_files_metas = list(map(lambda p: read_wav_file_meta(p), absolute_files_paths))
+        wav_absolute_file_paths = [f for f in absolute_files_paths if extract_extension(f) == "wav"]
+        audio_files_metas = list(map(lambda p: read_wav_file_meta(p), wav_absolute_file_paths))
         return ApiResponse(HttpStatusCode.ok, audio_files_metas)
 
     def delete(self, request: ApiRequest):
@@ -32,6 +33,10 @@ class AudioTagApi(AudiopyleRestApi):
     def get(self, request: ApiRequest):
         audio_file_name = request.query_params.get("file")
         self.logger.info("Reading ID3 tags of {}...".format(audio_file_name))
-        audio_file_absolute_path = concatenate_paths(AUDIO_FILES_DIR, audio_file_name)
-        id3_tag = read_id3_tag(audio_file_absolute_path)
-        return ApiResponse(HttpStatusCode.ok, id3_tag.serialize())
+        if extract_extension(audio_file_name) == "mp3":
+            audio_file_absolute_path = concatenate_paths(AUDIO_FILES_DIR, audio_file_name)
+            id3_tag = read_id3_tag(audio_file_absolute_path)
+            return ApiResponse(HttpStatusCode.ok, id3_tag.serialize())
+        else:
+            return ApiResponse(HttpStatusCode.bad_request,
+                               "Can not read tags from non-mp3 file: {}".format(audio_file_name))
