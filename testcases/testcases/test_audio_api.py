23ec0c1d18b96b7e39d3667d97884c5a51498d5a
emkor93 <emkor93@gmail.com>
2017-11-12 15:24:12 +0100

    added integration test for audio meta api

--- testcases/testcases/test_audio_api.py
+++ testcases/testcases/test_audio_api.py
@@ -21,3 +21,21 @@ class AudioApiTest(TestCase):
         assert_that(actual_response[0].get("created_on")).is_not_empty()
         assert_that(actual_response[0].get("last_modification")).is_not_empty()
         assert_that(actual_response[0].get("last_access")).is_not_empty()
+
+
+class AudioMetaApiTest(TestCase):
+    def setUp(self):
+        self.audio_api_url = "http://{}:8080/audio/meta".format(get_service_host_name("coordinator"))
+        self.file_name = "102bpm_drum_loop_mono_44.1k.mp3"
+
+    def test_should_return_correct_mp3_meta(self):
+        expected_status_code = 200
+        response = requests.get(url="{}?file={}".format(self.audio_api_url, self.file_name))
+        assert_that(response.status_code).is_equal_to(expected_status_code)
+        actual_response = response.json()
+        assert_that(actual_response).is_not_empty()
+        assert_that(actual_response.get("channels_count")).is_equal_to(1)
+        assert_that(actual_response.get("sample_rate")).is_equal_to(44100)
+        assert_that(actual_response.get("file_size_bytes")).is_equal_to(39044)
+        assert_that(actual_response.get("length_sec")).is_between(2.4, 2.5)
+        assert_that(actual_response.get("bit_rate_kbps")).is_between(127, 129)
