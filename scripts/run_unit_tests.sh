1aa69801d23adbde910834459edf7960a139a67f
emkor93 <emkor93@gmail.com>
2017-11-18 12:22:12 +0100

    added tox as test framework to commons

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-cd ./commons && py.test -v --cov=commons && cd ..
-cd ./coordinator && py.test -v --cov=coordinator && cd ..
-cd ./extractor && py.test -v --cov=extractor && cd ..
+cd ./commons && tox && cd ..
