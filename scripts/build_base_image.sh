ff06328b7dfc565b477202d5b29880141a6ed23e
emkor93 <emkor93@gmail.com>
2018-02-05 23:13:34 +0100

    modified build base image script

--- scripts/build_base_image.sh
+++ scripts/build_base_image.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-docker build -t endlessdrones/audiopyle-base ./base
+# check commit hash of current ./base directory
+base_image_commit_hash=$(git log -n 1 --format="%H" -- ./base)
+
+# try pulling image, save if image was found (status 0) or not (status 1)
+pull_by_commit_hash_status=$(docker pull endlessdrones/audiopyle-base:${base_image_commit_hash})
+
+# build the image only when pull failed
+if [ "$pull_by_commit_hash_status" = 1 ]; then
+    echo "Could not download image for commit hash ${base_image_commit_hash}, building..."
+    docker build -t endlessdrones/audiopyle-base:latest -t endlessdrones/audiopyle-base:${base_image_commit_hash} ./base
+else
+    echo "Downloaded image with commit hash ${base_image_commit_hash}, omitting build"
+fi
