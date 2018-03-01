d9352d4ee225922f31fe91390c22826d297c3b6b
emkor93 <emkor93@gmail.com>
2018-03-01 18:50:20 +0100

    renamed parse env var function, corrected the way logger is accessed

--- commons/commons/test/test_env_variable.py
+++ commons/commons/test/test_env_variable.py
@@ -2,7 +2,7 @@ import unittest
 
 from assertpy import assert_that
 
-from commons.utils.env_var import get_environment_variable
+from commons.utils.env_var import read_env_var
 
 
 class OsEnvVariableAccessingTest(unittest.TestCase):
@@ -10,15 +10,15 @@ class OsEnvVariableAccessingTest(unittest.TestCase):
         pass
 
     def test_should_read_existing_variable(self):
-        home_value = get_environment_variable("HOME", str)
+        home_value = read_env_var("HOME", str)
         assert_that(home_value).is_not_empty().starts_with("/")
 
     def test_should_fallback_to_default_on_non_existing_variable(self):
         default_value = "some_default"
-        non_existing_value = get_environment_variable("NON_EXSTING_VARIABLE", str, default_value)
+        non_existing_value = read_env_var("NON_EXSTING_VARIABLE", str, default_value)
         assert_that(non_existing_value).is_equal_to(default_value)
 
     def test_should_fallback_to_default_on_non_wrong_variable_type(self):
         default_value = 1.1
-        wrong_type_variable = get_environment_variable("HOME", float, default_value)
+        wrong_type_variable = read_env_var("HOME", float, default_value)
         assert_that(wrong_type_variable).is_equal_to(default_value)
--- commons/commons/utils/env_var.py
+++ commons/commons/utils/env_var.py
@@ -1,4 +1,5 @@
 import os
+
 from typing import Text, Type, Any, Optional
 
 from commons.utils.logger import get_logger
@@ -6,18 +7,18 @@ from commons.utils.logger import get_logger
 logger = get_logger()
 
 
-def get_environment_variable(variable_name: Text, expected_type: Type, default: Optional[Any] = None):
-    env_var = os.environ.get(variable_name)
+def read_env_var(var_name: Text, expected_type: Type, default: Optional[Any] = None):
+    env_var = os.environ.get(var_name)
     if env_var is not None:
         try:
             return expected_type(env_var)
         except ValueError as e:
-            logger.warning("Could not cast {}={} to {}: {}. Using default: {}".format(variable_name, env_var,
+            logger.warning("Could not cast {}={} to {}: {}. Using default: {}".format(var_name, env_var,
                                                                                       expected_type, e, default))
             return default
         except Exception as e:
-            logger.error("Error on reading variable {}: {}. Using default: {}".format(variable_name, e, default))
+            logger.error("Error on reading variable {}: {}. Using default: {}".format(var_name, e, default))
             return default
     else:
-        logger.warning("Variable {} was not set. Using default: {}".format(variable_name, default))
+        logger.warning("Variable {} was not set. Using default: {}".format(var_name, default))
         return default
