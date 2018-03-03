a00ca7a53a5b712f4316dcc0a7a401146f25fb44
emkor93 <emkor93@gmail.com>
2018-03-03 17:14:13 +0100

    changed URL parameter mapping in API

--- coordinator/coordinator/app.py
+++ coordinator/coordinator/app.py
@@ -3,7 +3,7 @@ from logging import Logger
 from flask import Flask
 
 from commons.utils.logger import setup_logger, get_logger
-from coordinator.api.audio_file import AudioFileApi, TmpAudioApi
+from coordinator.api.audio_file import AudioFileApi
 from coordinator.api.audio_meta import AudioMetaApi, AudioTagApi
 from coordinator.api.automation import AutomationApi
 from coordinator.api.plugin import PluginApi
@@ -21,14 +21,13 @@ def main():
 
 
 def start_app(logger: Logger, host: str, port: int, debug: bool = False):
-    app.add_url_rule("/", view_func=CoordinatorApi.as_view('coordinator_api', logger=logger))
-    app.add_url_rule("/plugin", view_func=PluginApi.as_view('plugin_api', logger=logger))
-    app.add_url_rule("/audio", view_func=AudioFileApi.as_view('audio_api', logger=logger))
-    app.add_url_rule("/audio/tmp", view_func=TmpAudioApi.as_view('audio_tmp_api', logger=logger))
-    app.add_url_rule("/audio/meta", view_func=AudioMetaApi.as_view('audio_meta_api', logger=logger))
-    app.add_url_rule("/audio/tag", view_func=AudioTagApi.as_view('audio_tag_api', logger=logger))
-    app.add_url_rule("/extraction", view_func=ExtractionApi.as_view('extraction_api', logger=logger))
     app.add_url_rule("/automation", view_func=AutomationApi.as_view('automation_api', logger=logger))
+    app.add_url_rule("/extraction", view_func=ExtractionApi.as_view('extraction_api', logger=logger))
+    app.add_url_rule("/plugin", view_func=PluginApi.as_view('plugin_api', logger=logger))
+    app.add_url_rule("/audio/meta/<identifier>", view_func=AudioMetaApi.as_view('audio_meta_api', logger=logger))
+    app.add_url_rule("/audio/tag/<identifier>", view_func=AudioTagApi.as_view('audio_tag_api', logger=logger))
+    app.add_url_rule("/audio", view_func=AudioFileApi.as_view('audio_api', logger=logger))
+    app.add_url_rule("/", view_func=CoordinatorApi.as_view('coordinator_api', logger=logger))
     app.run(host=host, port=port, debug=debug)
 
 
