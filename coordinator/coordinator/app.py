d1db15ef2752c47d82d821fc1b7c8cf1b34cc3a8
emkor93 <emkor93@gmail.com>
2018-03-05 19:41:56 +0100

    reworked plugin APIs

--- coordinator/coordinator/app.py
+++ coordinator/coordinator/app.py
@@ -7,7 +7,7 @@ from commons.utils.file_system import AUDIO_FILES_DIR
 from commons.utils.logger import setup_logger, get_logger
 from coordinator.api.audio_file import AudioFileListApi, AudioFileDetailApi
 from coordinator.api.automation import AutomationApi
-from coordinator.api.plugin import PluginListApi
+from coordinator.api.plugin import PluginListApi, PluginDetailApi
 from coordinator.api.root import CoordinatorApi
 from coordinator.api.extraction import ExtractionApi, ResultApi
 
@@ -26,7 +26,12 @@ def start_app(logger: Logger, host: str, port: int, debug: bool = False):
     app.add_url_rule("/automation", view_func=AutomationApi.as_view('automation_api', logger=logger))
     app.add_url_rule("/extraction", view_func=ExtractionApi.as_view('extraction_api', logger=logger))
     app.add_url_rule("/result/<task_id>", view_func=ResultApi.as_view('result_api', logger=logger))
-    app.add_url_rule("/plugin", view_func=PluginListApi.as_view('plugin_api', logger=logger))
+    app.add_url_rule("/plugin/<vendor>/<name>",
+                     view_func=PluginDetailApi.as_view('plugin_detail_api',
+                                                       logger=logger))
+    app.add_url_rule("/plugin",
+                     view_func=PluginListApi.as_view('plugin_list_api',
+                                                     logger=logger))
     app.add_url_rule("/audio/<identifier>",
                      view_func=AudioFileDetailApi.as_view('audio_detail_api',
                                                           file_store=audio_file_store,
