185045c2ab246c5bd669a2e4788e2501c8ccb445
emkor93 <emkor93@gmail.com>
2018-03-07 18:21:47 +0100

    corrected test cases for audio API

--- testcases/testcases/test_audio_api.py
+++ testcases/testcases/test_audio_api.py
@@ -9,33 +9,25 @@ from testcases.utils import get_api_host
 class AudioApiTest(TestCase):
     def setUp(self):
         self.audio_api_url = "http://{}:8080/audio".format(get_api_host())
+        self.audio_file = "102bpm_drum_loop"
 
     def test_should_list_audio_file(self):
         expected_status_code = 200
-        expected_audio_file_count = 2
+        expected_audio_file_count = 1
         response = requests.get(url=self.audio_api_url)
         assert_that(response.status_code).is_equal_to(expected_status_code)
         actual_response = response.json()
         assert_that(actual_response).is_length(expected_audio_file_count)
-        assert_that(actual_response[0].get("file_name")).is_not_none()
-        assert_that(actual_response[0].get("created_on")).is_not_none()
-        assert_that(actual_response[0].get("last_modification")).is_not_none()
-        assert_that(actual_response[0].get("last_access")).is_not_none()
+        assert_that(actual_response[0]).is_equal_to(self.audio_file)
 
-
-class AudioMetaApiTest(TestCase):
-    def setUp(self):
-        self.audio_api_url = "http://{}:8080/audio/meta".format(get_api_host())
-        self.file_name = "102bpm_drum_loop.mp3"
-
-    def test_should_return_correct_mp3_meta(self):
+    def test_should_list_file_details(self):
         expected_status_code = 200
-        response = requests.get(url="{}/{}".format(self.audio_api_url, self.file_name))
+        response = requests.get(url="{}/{}".format(self.audio_api_url, self.audio_file))
         assert_that(response.status_code).is_equal_to(expected_status_code)
+
         actual_response = response.json()
-        assert_that(actual_response).is_not_empty()
-        assert_that(actual_response.get("channels_count")).is_equal_to(1)
-        assert_that(actual_response.get("sample_rate")).is_equal_to(44100)
-        assert_that(actual_response.get("file_size_bytes")).is_equal_to(39044)
-        assert_that(actual_response.get("length_sec")).is_between(2.4, 2.5)
-        assert_that(actual_response.get("bit_rate_kbps")).is_between(127, 129)
+        assert_that(actual_response["file_name"]).is_equal_to("{}.{}".format(self.audio_file, "mp3"))
+        assert_that(actual_response["size"]).is_equal_to(39044)
+        assert_that(actual_response["created_on"]).is_not_none()
+        assert_that(actual_response["last_access"]).is_not_none()
+        assert_that(actual_response["last_modification"]).is_not_none()
