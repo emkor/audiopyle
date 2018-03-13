1d45c65ef399f705dc4d987cb974caa95264e9ed
emkor93 <emkor93@gmail.com>
2018-03-13 22:43:27 +0100

    moved installing as pymysql, added host and port parameters so code is more testable

--- commons/commons/db/engine.py
+++ commons/commons/db/engine.py
@@ -15,28 +15,28 @@ _ENGINE = None  # type: Optional[Engine]
 _SESSION_MAKER = None  # type: Optional[sessionmaker]
 
 
-def get_db_engine() -> Engine:
+def get_db_engine(host: str = None, port: int = None) -> Engine:
+    pymysql.install_as_MySQLdb()
     global _ENGINE
     if _ENGINE is None:
-        db_connection_string = _build_mysql_conn_string()
+        db_connection_string = _build_mysql_conn_string(host, port)
         logger.info("Creating SQL Alchemy DB engine with {} connection string...".format(db_connection_string))
         _ENGINE = create_engine(db_connection_string, echo=True)
     return _ENGINE
 
 
-def get_db_session_maker() -> sessionmaker:
+def get_db_session_maker(db_engine: Engine = None) -> sessionmaker:
     global _SESSION_MAKER
     if _SESSION_MAKER is None:
         logger.info("Creating SQL Alchemy sessions maker...")
-        _SESSION_MAKER = sessionmaker(bind=get_db_engine())
+        _SESSION_MAKER = sessionmaker(bind=db_engine or get_db_engine())
     return _SESSION_MAKER
 
 
 def create_db_tables() -> None:
-    pymysql.install_as_MySQLdb()
     ENTITY_BASE.metadata.create_all(get_db_engine(), checkfirst=True)
 
 
-def _build_mysql_conn_string() -> str:
-    return "mysql://audiopyle:audiopyle@{}:{}/audiopyle".format(read_env_var("MYSQL_SERVICE_HOST", str),
-                                                                read_env_var("MYSQL_SERVICE_PORT", int))
+def _build_mysql_conn_string(host: str = None, port: int = None) -> str:
+    return "mysql://audiopyle:audiopyle@{}:{}/audiopyle".format(host or read_env_var("MYSQL_SERVICE_HOST", str),
+                                                                port or read_env_var("MYSQL_SERVICE_PORT", int))
