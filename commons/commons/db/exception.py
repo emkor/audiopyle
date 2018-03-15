115c4251b80d146d4deb167ab6ffec6fdf62a21c
emkor93 <emkor93@gmail.com>
2018-03-15 13:55:10 +0100

    added description to repostiry exceptions

--- commons/commons/db/exception.py
+++ commons/commons/db/exception.py
@@ -1,10 +1,13 @@
 class RepositoryException(Exception):
+    """Generic repository exception"""
     pass
 
 
 class EntityNotFound(RepositoryException):
+    """Raised when calling for single entity and the entity was not found"""
     pass
 
 
 class DuplicateEntity(RepositoryException):
+    """Raised when doing insert and violating unique constraint"""
     pass
