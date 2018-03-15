3c8939d2ac5a7e704e9c9602fd0960f2eaf9d52b
emkor93 <emkor93@gmail.com>
2018-03-15 09:07:34 +0100

    overridden some representation methods so compressed data model does not print binary stuff on str()

--- commons/commons/models/compressed_feature.py
+++ commons/commons/models/compressed_feature.py
@@ -1,3 +1,5 @@
+from typing import Text, Any
+
 from enum import Enum
 
 from commons.abstractions.model import Model
@@ -14,3 +16,17 @@ class CompressedFeatureDTO(Model):
         self.task_id = task_id
         self.compression = compression
         self.data = data
+
+    def __str__(self) -> Text:
+        return "<{}: {} {}>".format(self.__class__.__name__,
+                                    {"task_id": self.task_id, "compression": self.compression},
+                                    self.size_humanized())
+
+    def __repr__(self) -> Text:
+        return self.__str__()
+
+    def __eq__(self, other: Any) -> bool:
+        return self.task_id == other.task_id and self.compression == other.compression
+
+    def __hash__(self) -> int:
+        return hash((self.task_id, self.compression))
