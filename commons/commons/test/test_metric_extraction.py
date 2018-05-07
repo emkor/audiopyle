1f23343f254e6d28c4f3713271e9c86c6b84264f
emkor93 <emkor93@gmail.com>
2018-05-07 10:28:03 +0200

    added tests for single value extractions

--- commons/commons/test/test_metric_extraction.py
+++ commons/commons/test/test_metric_extraction.py
@@ -3,12 +3,13 @@ from unittest import TestCase
 from assertpy import assert_that
 from numpy import array, float32
 
-from commons.models.feature import VampyConstantStepFeature
-from commons.models.metric import NoneTransformation, SelectRowTransformation
+from commons.models.feature import VampyConstantStepFeature, VampyVariableStepFeature, StepFeature
+from commons.models.metric import NoneTransformation, SelectRowTransformation, SingleValueTransformation
 
 
 class ConstantStepFeatureMetricTransformationTest(TestCase):
     def setUp(self):
+        self.single_dimensional_feature = array([0.38888031], dtype=float32)
         self.two_dimensional_feature = array([0.38888031, 0.3144314, 0.46564227, 0.31890243, 0.22512659], dtype=float32)
         self.three_dimensional_feature = array([[0.38888031, 0.3144314],
                                                 [0.46564227, 0.31890243],
@@ -16,6 +17,10 @@ class ConstantStepFeatureMetricTransformationTest(TestCase):
         self.constant_step_2d_feature = VampyConstantStepFeature("", time_step=0.1, matrix=self.two_dimensional_feature)
         self.constant_step_3d_feature = VampyConstantStepFeature("", time_step=0.1,
                                                                  matrix=self.three_dimensional_feature)
+        self.variable_step_single_value_feature = VampyVariableStepFeature("", [StepFeature(timestamp=0.0,
+                                                                                            values=array([519.51]))])
+        self.constant_step_single_value_feature = VampyConstantStepFeature("", time_step=0.1,
+                                                                           matrix=self.single_dimensional_feature)
 
     def test_should_extract_using_none_transformation_on_2d_feature(self):
         none_transformation = NoneTransformation()
@@ -34,3 +39,10 @@ class ConstantStepFeatureMetricTransformationTest(TestCase):
         assert_that(first_row_vector[1]).is_equal_to(self.three_dimensional_feature[1][0])
         assert_that(second_row_vector[0]).is_equal_to(self.three_dimensional_feature[0][1])
         assert_that(second_row_vector[1]).is_equal_to(self.three_dimensional_feature[1][1])
+
+    def test_should_extract_using_single_value_transformation(self):
+        single_value_transformation = SingleValueTransformation()
+        single_value_from_variable_step_feature = single_value_transformation.call(self.variable_step_single_value_feature)
+        single_value_from_constant_step_feature = single_value_transformation.call(self.constant_step_single_value_feature)
+        assert_that(single_value_from_variable_step_feature).is_length(2)
+        assert_that(single_value_from_constant_step_feature).is_length(2)
