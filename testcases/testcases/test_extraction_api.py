1c5723759ba2a8e127787b617023dedc960d91b4
emkor93 <emkor93@gmail.com>
2018-05-11 12:17:57 +0200

    extended integration tests for extraction

--- testcases/testcases/test_extraction_api.py
+++ testcases/testcases/test_extraction_api.py
@@ -43,10 +43,52 @@ class CoordinatorApiTest(TestCase):
         assert_that(json_status_response["status"]).is_equal_to(TaskStatus.done.value)
         assert_that(json_status_response["task_id"]).is_equal_to(extraction_task_id)
 
+        # retrieve and assert request params
+        result_request_response = requests.get(url="{}/{}".format(self.result_api_url, extraction_task_id))
+        assert_that(result_request_response.status_code).is_equal_to(retrieval_expected_status_code)
+        result_request_response_json = result_request_response.json()
+        assert_that(result_request_response_json["audio_meta"]).is_equal_to({
+            "bit_rate_kbps": 128.0,
+            "channels_count": 1,
+            "file_name": "102bpm_drum_loop.mp3",
+            "file_size_bytes": 39044,
+            "length_sec": 2.403,
+            "sample_rate": 44100
+        })
+        assert_that(result_request_response_json["id3_tag"]).is_equal_to({
+            "album": "Unknown Album",
+            "artist": "Unknown Artist",
+            "date": 2017,
+            "genre": "Unknown Genre",
+            "title": "Unknown Title",
+            "track": 1
+        })
+        assert_that(result_request_response_json["plugin"]).is_equal_to({
+            "library_file_name": "vamp-example-plugins.so",
+            "name": "amplitudefollower",
+            "output": "amplitude",
+            "vendor": "vamp-example-plugins"
+        })
+        assert_that(result_request_response_json["plugin_config"]["block_size"]).is_equal_to(4096)
+        assert_that(result_request_response_json["plugin_config"]["step_size"]).is_equal_to(4096)
+        assert_that(result_request_response_json["task_id"]).is_not_empty()
+
         # retrieve data, meta and stats of an extraction
         result_data_response = requests.get(url="{}/{}/data".format(self.result_api_url, extraction_task_id))
         assert_that(result_data_response.status_code).is_equal_to(retrieval_expected_status_code)
+        result_data_response_json = result_data_response.json()
+        assert_that(result_data_response_json["matrix"]).is_length(26)
+        assert_that(result_data_response_json["time_step"]).is_between(0.092, 0.093)
+        assert_that(result_data_response_json["task_id"]).is_not_empty()
+
         result_meta_response = requests.get(url="{}/{}/meta".format(self.result_api_url, extraction_task_id))
         assert_that(result_meta_response.status_code).is_equal_to(retrieval_expected_status_code)
+        result_meta_response_json = result_meta_response.json()
+        assert_that(result_meta_response_json["data_shape"]).is_equal_to([26, 1, 0])
+        assert_that(result_meta_response_json["feature_type"]).is_equal_to("constant_step")
+
         result_stats_response = requests.get(url="{}/{}/stats".format(self.result_api_url, extraction_task_id))
         assert_that(result_stats_response.status_code).is_equal_to(retrieval_expected_status_code)
+        result_stats_response_json = result_stats_response.json()
+        assert_that(result_stats_response_json["total_time"]).is_between(0.01, 1.5)
+        assert_that(result_stats_response_json["task_id"]).is_not_empty()
