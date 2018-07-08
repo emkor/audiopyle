d4ab33f05f383d8b062b5189294efbf4901a1530
emkor93 <emkor93@gmail.com>
2018-07-08 11:55:22 +0200

    corrected env var parsing tests

--- commons/commons/test/test_env_variable.py
+++ commons/commons/test/test_env_variable.py
@@ -6,9 +6,6 @@ from commons.utils.env_var import read_env_var, _select_value
 
 
 class OsEnvVariableAccessingTest(unittest.TestCase):
-    def setUp(self):
-        pass
-
     def test_should_read_existing_variable(self):
         home_value = read_env_var("HOME", str)
         assert_that(home_value).is_not_empty().starts_with("/")
@@ -24,9 +21,9 @@ class OsEnvVariableAccessingTest(unittest.TestCase):
         assert_that(wrong_type_variable).is_equal_to(default_value)
 
     def test_should_cast_int_of_0_to_false(self):
-        wrong_type_variable = _select_value("DOESNT_MATTER", bool, default=True, actual=0)
+        wrong_type_variable = _select_value("DOESNT_MATTER", bool, default=True, actual="0")
         assert_that(wrong_type_variable).is_equal_to(False)
 
     def test_should_cast_int_of_1_to_true(self):
-        wrong_type_variable = _select_value("DOESNT_MATTER", bool, default=False, actual=1)
+        wrong_type_variable = _select_value("DOESNT_MATTER", bool, default=False, actual="1")
         assert_that(wrong_type_variable).is_equal_to(True)
--- commons/commons/utils/env_var.py
+++ commons/commons/utils/env_var.py
@@ -8,20 +8,23 @@ logger = get_logger()
 
 
 def read_env_var(var_name: Text, expected_type: Type, default: Optional[Any] = None) -> Any:
-    return _select_value(var_name, expected_type, default, os.environ.get(var_name))
+    actual_value = os.environ.get(var_name)
+    if actual_value is not None:
+        return _select_value(var_name, expected_type, default, actual_value)
+    else:
+        logger.warning("Variable {} was not set. Using default: {}".format(var_name, default))
+        return default
 
 
 def _select_value(var_name: str, expected_type: Type, default: Optional[Any], actual: Optional[Any]) -> Any:
-    if actual is not None:
-        try:
-            return expected_type(actual)
-        except ValueError as e:
-            logger.warning("Could not cast {}={} to {}: {}. Using default: {}".format(var_name, actual,
-                                                                                      expected_type, e, default))
-            return default
-        except Exception as e:
-            logger.error("Error on reading variable {}: {}. Using default: {}".format(var_name, e, default))
-            return default
-    else:
-        logger.warning("Variable {} was not set. Using default: {}".format(var_name, default))
+    try:
+        if expected_type == bool:
+            return bool(int(actual))
+        return expected_type(actual)
+    except ValueError as e:
+        logger.warning("Could not cast {}={} to {}: {}. Using default: {}".format(var_name, actual,
+                                                                                  expected_type, e, default))
+        return default
+    except Exception as e:
+        logger.error("Error on reading variable {}: {}. Using default: {}".format(var_name, e, default))
         return default
