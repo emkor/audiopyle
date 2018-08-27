60d9f962046e500dce2566139fcb59295c4aa16f
emkor93 <emkor93@gmail.com>
2018-08-27 11:46:05 +0200

    added documentation for audio file API endpoints

--- audiopyle/api/audio_file.py
+++ audiopyle/api/audio_file.py
@@ -32,9 +32,56 @@ class AudioFileDetailApi(AbstractRestApi):
         self.file_store = file_store
 
     def get(self, **kwargs) -> str:
+        """Returns audio file details
+        ---
+        definitions:
+            FileDetail:
+                type: object
+                properties:
+                    file_name:
+                        type: "string"
+                    size:
+                        description: "File size in bytes"
+                        type: "integer"
+                    created_on:
+                        description: "Unix creation date (UTC) in format: %Y-%m-%d %H:%M:%S"
+                        type: "string"
+                    last_modification:
+                        description: "Unix last modification date (UTC) in format: %Y-%m-%d %H:%M:%S"
+                        type: "string"
+                    last_access:
+                        description: "Unix last access date (UTC) in format: %Y-%m-%d %H:%M:%S"
+                        type: "string"
+        parameters:
+            -   name: "file_name"
+                in: path
+                required: "true"
+                type: "string"
+        responses:
+            200:
+                description: Details of given audio file
+                schema:
+                    $ref: '#/definitions/FileDetail'
+                examples:
+                    application/json: |-
+                        {
+                            created_on: "2018-08-16 08:58:49",
+                            file_name: "102bpm_drum_loop.flac",
+                            last_access: "2018-08-16 08:58:49",
+                            last_modification: "2018-08-16 08:58:49",
+                            size: 111690
+                        }
+            400:
+                description: file_name parameter was not provided in URL
+            404:
+                description: Can not find a file with given name
+        """
         api_request = build_request(request, **kwargs)
-        file_name = api_request.query_params["file_name"]
-        if self.file_store.exists(file_name):
+        file_name = api_request.query_params.get("file_name")
+        if file_name is None:
+            api_response = ApiResponse(HttpStatusCode.bad_request,
+                                       {"error": "Parameter file_name was not provided"})
+        elif self.file_store.exists(file_name):
             api_response = ApiResponse(HttpStatusCode.ok, self.file_store.meta(file_name).to_serializable())
         else:
             api_response = ApiResponse(HttpStatusCode.not_found,
