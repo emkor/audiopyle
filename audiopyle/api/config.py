452074963607081a9b208743e7c91597d2dbd0e6
emkor93 <emkor93@gmail.com>
2018-08-28 10:40:23 +0200

    added documentation for metric config and plugin config APIs

--- audiopyle/api/config.py
+++ audiopyle/api/config.py
@@ -13,6 +13,34 @@ class PluginActiveConfigApi(AbstractRestApi):
         self.plugin_config_provider = plugin_config_provider
 
     def get(self, **kwargs) -> str:
+        """Returns available plugin configuration per plugin
+        ---
+        responses:
+            200:
+                description: Content of plugin config JSON file
+                schema:
+                    type: object
+                    additionalProperties:
+                        type: object
+                        properties:
+                            block_size:
+                                type: integer
+                                nullable: true
+                            step_size:
+                                type: integer
+                                nullable: true
+                        additionalProperties: true
+                examples:
+                    application/json: |-
+                        {
+                            "vamp-example-plugins:amplitudefollower:amplitude": {
+                                "block_size":4096,
+                                "step_size":4096
+                            }
+                        }
+            404:
+                description: There was no plugin config file found
+        """
         api_request = build_request(request, **kwargs)
         try:
             api_response = ApiResponse(status_code=HttpStatusCode.ok, payload=self.plugin_config_provider.get_all())
@@ -28,6 +56,39 @@ class MetricActiveConfigApi(AbstractRestApi):
         self.metric_config_provider = metric_config_provider
 
     def get(self, **kwargs) -> str:
+        """Returns available plugin configuration per plugin
+        ---
+        responses:
+            200:
+                description: Content of metric config JSON file
+                schema:
+                    type: object
+                    additionalProperties:
+                        type: object
+                        properties:
+                            plugin:
+                                type: string
+                            transformation:
+                                type: object
+                                properties:
+                                    name:
+                                        type: string
+                                    kwargs:
+                                        type: object
+                                        additionalProperties: true
+                examples:
+                    application/json: |-
+                        {
+                            "bbc_energy_average_rms": {
+                                "plugin":"bbc-vamp-plugins:bbc-energy:average",
+                                "transformation": {
+                                    "name":"none"
+                                }
+                            }
+                        }
+            404:
+                description: There was no metric config file found
+        """
         api_request = build_request(request, **kwargs)
         try:
             api_response = ApiResponse(status_code=HttpStatusCode.ok, payload=self.metric_config_provider.get_all())
