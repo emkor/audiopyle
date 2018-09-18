a76dff8565e2462180a7b633c8e670a2a2c72719
emkor93 <emkor93@gmail.com>
2018-09-18 20:34:38 +0200

    implemented sending extraction requests and viewing extracted tasks

--- frontend/index.js
+++ frontend/index.js
@@ -6,14 +6,20 @@ var app = new Vue({
         api_status: 'not ok',
         audio_files: [],
         vamp_plugins: [],
+        requests: [],
         selected_file: null,
         selected_file_details: null,
         selected_plugin: null,
-        selected_plugin_details: null
+        selected_plugin_details: null,
+        selected_request: null,
+        selected_request_status: null,
+        selected_request_details: null,
+        requestPossible: false
     },
     methods: {
         fetchAudioDetails: function (event) {
             app.selected_file = event.currentTarget.name;
+            app.requestPossible = app.selected_file != null && app.selected_plugin != null;
             let audioUrl = API_HOST + '/audio/' + app.selected_file;
             fetch(audioUrl)
                 .then(function (response) {
@@ -25,6 +31,7 @@ var app = new Vue({
         },
         fetchPluginDetails: function (event) {
             app.selected_plugin = event.currentTarget.name;
+            app.requestPossible = app.selected_file != null && app.selected_plugin != null;
             let pluginUrl = API_HOST + '/plugin/' + app.selected_plugin.replace(/:/g, "/");
             fetch(pluginUrl)
                 .then(function (response) {
@@ -33,6 +40,54 @@ var app = new Vue({
                         .catch(reason => console.error("Could not parse response from " + pluginUrl + ": " + reason));
                 })
                 .catch(error => console.error("Could not fetch details for " + app.selected_plugin + ": " + error));
+        },
+        fetchRequestDetails: function (event) {
+            app.selected_request = event.currentTarget.name;
+            let requestStatusUrl = API_HOST + '/request/' + app.selected_request + '/status';
+            fetch(requestStatusUrl)
+                .then(function (response) {
+                    response.json()
+                        .then(function (value) {
+                            app.selected_request_status = value.status;
+                            if (app.selected_request_status === 'done') {
+                                let requestDetailsUrl = API_HOST + '/request/' + app.selected_request;
+                                fetch(requestDetailsUrl)
+                                    .then(function (response) {
+                                        response.json()
+                                            .then(value => app.selected_request_details = value)
+                                            .catch(reason => console.error("Could not parse response from " + requestDetailsUrl + ": " + reason));
+                                    })
+                                    .catch(error => console.error("Could not fetch details for " + app.selected_request + ": " + error));
+                            } else {
+                                console.info("Omitting fetching request details, because status is " + app.selected_request_status);
+                            }
+
+                        })
+                        .catch(reason => console.error("Could not parse response from " + requestStatusUrl + ": " + reason));
+                })
+                .catch(error => console.error("Could not fetch status for " + app.selected_request + ": " + error));
+        },
+        sendRequest: function (event) {
+            let sendRequestUrl = API_HOST + '/request';
+            let payload = {
+                "audio_file_name": app.selected_file,
+                "plugin_full_key": app.selected_plugin,
+                "plugin_config": null,
+                "metric_config": null
+            };
+            let request = {
+                method: "POST",
+                headers: {"Content-Type": "application/json; charset=utf-8"},
+                redirect: "follow",
+                body: JSON.stringify(payload)
+            };
+            fetch(sendRequestUrl, request)
+                .then(function (response) {
+                    response.json()
+                        .then(value => app.requests.push(value.task_id))
+                        .catch(reason => console.error("Could not parse response from " + sendRequestUrl + ": " + reason));
+                })
+                .catch(error => console.error("Could not fetch details for " + app.selected_request + ": " + error));
         }
     }
 });
@@ -61,6 +116,14 @@ function fetchInputFromApi(apiHost) {
                 .catch(reason => console.error("Could not parse plugin list: " + reason));
         })
         .catch(error => console.error("Could not fetch from /plugin: " + error));
+
+    fetch(apiHost + '/request')
+        .then(function (response) {
+            response.json()
+                .then(value => app.requests = value)
+                .catch(reason => console.error("Could not parse request list: " + reason));
+        })
+        .catch(error => console.error("Could not fetch from /request: " + error));
 }
 
 fetchInputFromApi(API_HOST);
