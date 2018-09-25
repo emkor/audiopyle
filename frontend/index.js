e4424ffee9479db3b975b634aa809d90b348ac0a
emkor93 <emkor93@gmail.com>
2018-09-25 19:56:59 +0200

    split JS file into lib and app, added reload possibility on requests tab

--- frontend/index.js
+++ frontend/index.js
@@ -1,28 +1,3 @@
-API_HOST = 'http://localhost:8080';
-
-function fetchJson(url, responseJsonHandler, errorHandler) {
-    // on success, calls responseJsonHandler() with response JSON, on failure - calls errorHandler() with error message
-    fetch(url)
-        .then(function (response) {
-            if (response.ok) {
-                response.json()
-                    .then(value => responseJsonHandler(value))
-                    .catch(function (reason) {
-                        console.error("Could not parse response from " + url + ": " + reason);
-                        errorHandler(reason);
-                    });
-            }
-            else {
-                console.error("Response from " + url + " was " + response.status + " (" + response.statusText + ")");
-                errorHandler(response.statusText);
-            }
-        })
-        .catch((function (reason) {
-            console.error("Could not parse response from " + url + ": " + reason);
-            errorHandler(reason);
-        }));
-}
-
 var app = new Vue({
     el: '#app',
     data: {
@@ -49,95 +24,23 @@ var app = new Vue({
         }
     },
     methods: {
-        fetchAudioDetails: function (event) {
-            app.selected_file = event.currentTarget.name;
-            app.selected_file_details = null;
-            fetchJson(API_HOST + '/audio/' + encodeURIComponent(app.selected_file),
-                v => app.selected_file_details = v,
-                e => app.selected_file_details = null);
-        },
-        fetchPluginDetails: function (event) {
-            app.selected_plugin = event.currentTarget.name;
-            app.selected_plugin_details = null;
-            let encodedPluginUrl = encodeURIComponent(app.selected_plugin.replace(/:/g, "/"));
-            fetchJson(API_HOST + '/plugin/' + encodedPluginUrl,
-                function (v) {
-                    app.selected_plugin_details = v;
-                    fetchJson(API_HOST + '/config/plugin/' + encodedPluginUrl + "/metric",
-                            m => app.selected_plugin_metrics = m,
-                            e => app.selected_plugin_metrics = []);
-                    fetchJson(API_HOST + '/config/plugin/' + encodedPluginUrl,
-                        c => app.selected_plugin_config = c,
-                        e => app.selected_plugin_config = {})
-                },
-                e => app.selected_plugin_details = null);
-        },
-        fetchRequestDetails: function (event) {
-            app.selected_request = event.currentTarget.name;
-            app.selected_request_details = null;
-            fetchJson(API_HOST + '/request/' + encodeURIComponent(app.selected_request) + '/status',
-                function (value) {
-                    app.selected_request_status = value.status;
-                    if (app.selected_request_status === 'done') {
-                        fetchJson(API_HOST + '/request/' + app.selected_request,
-                            v => app.selected_request_details = v,
-                            e => app.selected_request_details = null);
-                    } else {
-                        console.info("Omitting fetching request details, because status is " + app.selected_request_status);
-                    }
-                },
-                function (e) {
-                    app.selected_request = null;
-                    app.selected_request_details = null;
-                });
-        },
-        sendRequest: function (event) {
-            let sendRequestUrl = API_HOST + '/request';
-            let payload = {
-                "audio_file_name": app.selected_file,
-                "plugin_full_key": app.selected_plugin,
-                "plugin_config": null,
-                "metric_config": null
-            };
-            let request = {
-                method: "POST",
-                headers: {"Content-Type": "application/json; charset=utf-8"},
-                redirect: "follow",
-                body: JSON.stringify(payload)
-            };
-            fetch(sendRequestUrl, request)
-                .then(function (response) {
-                    response.json()
-                        .then(value => app.requests.push(value.task_id))
-                        .catch(reason => console.error("Could not parse response from " + sendRequestUrl + ": " + reason));
-                })
-                .catch(error => console.error("Could not fetch details for " + app.selected_request + ": " + error));
-        },
-        resetSelection: function(event) {
-            app.selected_file = null;
-            app.selected_file_details = null;
-            app.selected_plugin = null;
-            app.selected_plugin_details = null;
+        fetchAudioDetails: event => fetchAudioDetails(event.currentTarget.name),
+        fetchPluginDetails: event => fetchPluginDetails(event.currentTarget.name),
+        fetchRequestDetails: event => fetchRequestDetails(event.currentTarget.name),
+        sendRequest: event => sendExtractionRequest(API_HOST + '/request', {
+            "audio_file_name": app.selected_file,
+            "plugin_full_key": app.selected_plugin,
+            "plugin_config": null,
+            "metric_config": null
+        }, function (event) { resetSelection(); }),
+        resetSelection: event => resetSelection(),
+        initData: function() {
+            featchApiStatus();
+            fetchAudioFiles();
+            fetchPluginList();
+            fetchRequestList();
         }
     }
 });
 
-function fetchInputFromApi(apiHost) {
-    fetchJson(apiHost + '/',
-        v => app.api_status = v.status,
-        e => app.api_status = "not ok! " + e);
-
-    fetchJson(apiHost + '/audio',
-        v => app.audio_files = v,
-        r => app.audio_files = []);
-
-    fetchJson(apiHost + '/plugin',
-        v => app.vamp_plugins = v,
-        e => app.vamp_plugins = []);
-
-    fetchJson(apiHost + '/request',
-        v => app.requests = v,
-        e => app.requests = []);
-}
-
-fetchInputFromApi(API_HOST);
+app.initData();
