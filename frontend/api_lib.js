789de7850bae7ad40fa6d766b635cb3d68a9b45f
emkor93 <emkor93@gmail.com>
2018-09-28 20:00:28 +0200

    split makeRequestApp and viewResultsApp from single Vue app

--- frontend/api_lib.js
+++ frontend/api_lib.js
@@ -24,62 +24,62 @@ function fetchJson(url, responseJsonHandler, errorHandler) {
 }
 
 function featchApiStatus() {
-    app.api_status = "...";
+    makeRequestApp.api_status = "...";
     fetchJson(API_HOST + '/',
-        v => app.api_status = v.status,
-        e => app.api_status = "not ok! " + e);
+        v => makeRequestApp.api_status = v.status,
+        e => makeRequestApp.api_status = "not ok! " + e);
 }
 
 function fetchAudioFiles() {
-    app.audio_files = [];
+    makeRequestApp.audio_files = [];
     fetchJson(API_HOST + '/audio',
-        v => app.audio_files = v,
-        r => app.audio_files = []);
+        v => makeRequestApp.audio_files = v,
+        r => makeRequestApp.audio_files = []);
 }
 
 function fetchPluginList() {
-    app.vamp_plugins = [];
+    makeRequestApp.vamp_plugins = [];
     fetchJson(API_HOST + '/plugin',
-        v => app.vamp_plugins = v,
-        e => app.vamp_plugins = []);
+        v => makeRequestApp.vamp_plugins = v,
+        e => makeRequestApp.vamp_plugins = []);
 }
 
 function fetchRequestList() {
-    app.requests = [];
+    viewResultsApp.requests = [];
     fetchJson(API_HOST + '/request',
-        v => app.requests = v,
-        e => app.requests = []);
+        v => viewResultsApp.requests = v,
+        e => viewResultsApp.requests = []);
 }
 
 function fetchAudioDetails(audioFileName) {
-    app.selected_file = audioFileName;
-    app.selected_file_details = null;
-    app.selected_file_tags = null;
-    fetchJson(API_HOST + '/audio/' + encodeURIComponent(app.selected_file),
-        v => app.selected_file_details = v,
-        e => app.selected_file_details = null);
-    fetchJson(API_HOST + '/audio/' + encodeURIComponent(app.selected_file) + '/tag',
-        v => app.selected_file_tags = v,
-        e => app.selected_file_tags = null);
+    makeRequestApp.selected_file = audioFileName;
+    makeRequestApp.selected_file_details = null;
+    makeRequestApp.selected_file_tags = null;
+    fetchJson(API_HOST + '/audio/' + encodeURIComponent(makeRequestApp.selected_file),
+        v => makeRequestApp.selected_file_details = v,
+        e => makeRequestApp.selected_file_details = null);
+    fetchJson(API_HOST + '/audio/' + encodeURIComponent(makeRequestApp.selected_file) + '/tag',
+        v => makeRequestApp.selected_file_tags = v,
+        e => makeRequestApp.selected_file_tags = null);
 }
 
 function fetchPluginDetails(pluginKey) {
-    app.selected_plugin = pluginKey;
-    app.selected_plugin_details = null;
-    app.selected_plugin_metrics = [];
-    app.selected_plugin_config = {};
-    let encodedPluginUrl = encodeURIComponent(app.selected_plugin.replace(/:/g, "/"));
+    makeRequestApp.selected_plugin = pluginKey;
+    makeRequestApp.selected_plugin_details = null;
+    makeRequestApp.selected_plugin_metrics = [];
+    makeRequestApp.selected_plugin_config = {};
+    let encodedPluginUrl = encodeURIComponent(makeRequestApp.selected_plugin.replace(/:/g, "/"));
     fetchJson(API_HOST + '/plugin/' + encodedPluginUrl,
         function (v) {
-            app.selected_plugin_details = v;
+            makeRequestApp.selected_plugin_details = v;
             fetchJson(API_HOST + '/config/plugin/' + encodedPluginUrl + "/metric",
-                m => app.selected_plugin_metrics = m,
-                e => app.selected_plugin_metrics = []);
+                m => makeRequestApp.selected_plugin_metrics = m,
+                e => makeRequestApp.selected_plugin_metrics = []);
             fetchJson(API_HOST + '/config/plugin/' + encodedPluginUrl,
-                c => app.selected_plugin_config = c,
-                e => app.selected_plugin_config = {})
+                c => makeRequestApp.selected_plugin_config = c,
+                e => makeRequestApp.selected_plugin_config = {})
         },
-        e => app.selected_plugin_details = null);
+        e => makeRequestApp.selected_plugin_details = null);
 }
 
 function sendExtractionRequest(sendRequestUrl, payload, responseHandler) {
@@ -95,59 +95,51 @@ function sendExtractionRequest(sendRequestUrl, payload, responseHandler) {
                 .then(value => responseHandler(value))
                 .catch(reason => console.error("Could not parse response from " + sendRequestUrl + ": " + reason));
         })
-        .catch(error => console.error("Could not fetch request " + app.selected_request + ": " + error));
+        .catch(error => console.error("Could not fetch request " + makeRequestApp.selected_request + ": " + error));
 }
 
 function fetchRequest(requestId) {
-    app.selected_request = requestId;
-    app.selected_request_details = null;
-    app.selected_request_metrics = [];
-    app.selected_request_selected_metric_name = null;
-    app.selected_request_selected_metric_value = null;
-    fetchJson(API_HOST + '/request/' + encodeURIComponent(app.selected_request) + '/status',
+    viewResultsApp.selected_request = requestId;
+    viewResultsApp.selected_request_details = null;
+    viewResultsApp.selected_request_metrics = [];
+    viewResultsApp.selected_request_selected_metric_name = null;
+    viewResultsApp.selected_request_selected_metric_value = null;
+    fetchJson(API_HOST + '/request/' + encodeURIComponent(viewResultsApp.selected_request) + '/status',
         function (value) {
-            app.selected_request_status = value.status;
-            if (app.selected_request_status === 'done') {
-                fetchRequestDetails(app.selected_request);
-                fetchRequestMetricNames(app.selected_request);
+            viewResultsApp.selected_request_status = value.status;
+            if (viewResultsApp.selected_request_status === 'done') {
+                fetchRequestDetails(viewResultsApp.selected_request);
+                fetchRequestMetricNames(viewResultsApp.selected_request);
             }
             else {
-                console.info("Omitting fetching request details, because status is " + app.selected_request_status);
+                console.info("Omitting fetching request details, because status is " + viewResultsApp.selected_request_status);
             }
         },
         function (e) {
-            app.selected_request = null;
-            app.selected_request_details = null;
+            viewResultsApp.selected_request = null;
+            viewResultsApp.selected_request_details = null;
         });
 }
 
 function fetchRequestMetricValues(metric_name) {
-    app.selected_request_selected_metric_name = metric_name;
-    app.selected_request_selected_metric_value = null;
+    viewResultsApp.selected_request_selected_metric_name = metric_name;
+    viewResultsApp.selected_request_selected_metric_value = null;
     let url = API_HOST
-        + '/request/' + encodeURIComponent(app.selected_request)
-        + '/metric/' + encodeURIComponent(app.selected_request_selected_metric_name);
+        + '/request/' + encodeURIComponent(viewResultsApp.selected_request)
+        + '/metric/' + encodeURIComponent(viewResultsApp.selected_request_selected_metric_name);
     fetchJson(url,
-        v => app.selected_request_selected_metric_value = v,
-        e => app.selected_request_selected_metric_value = null)
+        v => viewResultsApp.selected_request_selected_metric_value = v,
+        e => viewResultsApp.selected_request_selected_metric_value = null)
 }
 
 function fetchRequestDetails(task_id) {
     fetchJson(API_HOST + '/request/' + task_id,
-        v => app.selected_request_details = v,
-        e => app.selected_request_details = null);
+        v => viewResultsApp.selected_request_details = v,
+        e => viewResultsApp.selected_request_details = null);
 }
 
 function fetchRequestMetricNames(task_id) {
     fetchJson(API_HOST + '/request/' + task_id + "/metric",
-        v => app.selected_request_metrics = v,
-        e => app.selected_request_metrics = []);
-}
-
-function resetSelection(event) {
-    app.selected_file = null;
-    app.selected_file_details = null;
-    app.selected_plugin = null;
-    app.selected_plugin_details = null;
-    app.selected_plugin_metrics = [];
+        v => viewResultsApp.selected_request_metrics = v,
+        e => viewResultsApp.selected_request_metrics = []);
 }
---
+++ frontend/app.js
@@ -0,0 +1,79 @@
+var makeRequestApp = new Vue({
+    el: '#makeRequest',
+    data: {
+        audio_files: [],
+        vamp_plugins: [],
+        selected_file: null,
+        selected_file_details: null,
+        selected_file_tags: null,
+        selected_plugin: null,
+        selected_plugin_details: null,
+        selected_plugin_config: {},
+        selected_plugin_metrics: []
+    },
+    computed: {
+        requestNotPossible() {
+            return this.selected_file == null || this.selected_plugin == null;
+        },
+        nothingSelected() {
+            return this.selected_file == null && this.selected_plugin == null;
+        }
+    },
+    methods: {
+        toMegabytes: function (bytes) {
+            return (bytes / (1024 * 1024)).toFixed(2);
+        },
+        fromIsoToHumanDate: function (isoDateString) {
+            let dateObj = new Date(isoDateString);
+            return dateObj.toLocaleString();
+        },
+        fetchAudioDetails: event => fetchAudioDetails(event.currentTarget.name),
+        fetchPluginDetails: event => fetchPluginDetails(event.currentTarget.name),
+        resetSelection: function (event) {
+            makeRequestApp.selected_file = null;
+            makeRequestApp.selected_file_details = null;
+            makeRequestApp.selected_plugin = null;
+            makeRequestApp.selected_plugin_details = null;
+            makeRequestApp.selected_plugin_metrics = [];
+        },
+        sendRequest: event => sendExtractionRequest(API_HOST + '/request', {
+            "audio_file_name": makeRequestApp.selected_file,
+            "plugin_full_key": makeRequestApp.selected_plugin,
+            "plugin_config": makeRequestApp.selected_plugin_config,
+            "metric_config": null
+        }, function (event) {
+            makeRequestApp.resetSelection();
+        }),
+        initData: function () {
+            fetchAudioFiles();
+            fetchPluginList();
+        }
+    }
+});
+
+var viewResultsApp = new Vue({
+    el: '#browseResults',
+    data: {
+        requests: [],
+        selected_request: null,
+        selected_request_status: null,
+        selected_request_details: null,
+        selected_request_metrics: [],
+        selected_request_selected_metric_name: null,
+        selected_request_selected_metric_value: null
+    },
+    methods: {
+        roundStat: function (number) {
+            return number.toFixed(3);
+        },
+        fetchRequestDetails: event => fetchRequest(event.currentTarget.name),
+        fetchRequestMetricDetails: event => fetchRequestMetricValues(event.currentTarget.name),
+        fetchRequests: event => fetchRequestList(),
+        initData: function () {
+            this.fetchRequests();
+        }
+    }
+});
+
+makeRequestApp.initData();
+viewResultsApp.initData();
